2e7d844d133500ee9adacfe55d05dc3a9a786d67
Honoour <hearful@icloud.com>
2021-06-13 00:14:25 +0900

    profile page 1

--- src/test/java/com/studyolle/demo/main/MainControllerTest.java
+++ src/test/java/com/studyolle/demo/main/MainControllerTest.java
@@ -1,14 +1,19 @@
 package com.studyolle.demo.main;
 
 
+import com.studyolle.demo.account.AccountRepository;
 import com.studyolle.demo.account.AccountService;
 import com.studyolle.demo.account.SignUpForm;
-import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -16,25 +21,69 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-// 사용할 수 없음 Junit 먼저관여 @RequiredArgsConstructor
+// @RequiredArgsConstructor 사용할 수 없음 Junit 먼저 관여 하기 때문
 public class MainControllerTest {
     @Autowired MockMvc mockMvc;
     @Autowired AccountService accountService;
+    @Autowired AccountRepository accountRepository;
 
-    @Test
-    void login_with_email() throws Exception {
+    @BeforeEach
+    void beforEach() {
         SignUpForm signUpForm = new SignUpForm();
         signUpForm.setNickname("jieun");
         signUpForm.setEmail("jieun@icloud.com");
         signUpForm.setPassword("12341234");
         accountService.processNewAccount(signUpForm);
-        
+    }
+
+    @AfterEach
+    void afterEach() {
+        accountRepository.deleteAll();
+    }
+
+    @DisplayName("이메일로 로그인 성공")
+    @Test
+    void login_with_email() throws Exception {
         mockMvc.perform(post("/login")
                 .param("username", "jieun@icloud.com")
                 .param("password", "12341234")
                 .with(csrf()))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/"));
+    }
+
+    @DisplayName("닉네임으로 로그인 성공")
+    @Test
+    void login_with_nickname() throws Exception {
+        mockMvc.perform(post("/login")
+                .param("username", "jieun@icloud.com")
+                .param("password", "12341234")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @DisplayName("로그인 실패")
+    @Test
+    void login_fail() throws Exception {
+        mockMvc.perform(post("/login")
+                .param("username", "wrong_username")
+                .param("password", "11111111")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?error"))
+                .andExpect(SecurityMockMvcResultMatchers.unauthenticated());
+    }
+
+    @WithMockUser
+    @DisplayName("로그아웃")
+    @Test
+    void logout() throws Exception {
+        mockMvc.perform(post("/logout")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(SecurityMockMvcResultMatchers.unauthenticated());
     }
 }
 
