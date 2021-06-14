2996023e1250b1336315ac8e505043f7bdf35541
Honoour <hearful@icloud.com>
2021-06-15 00:01:30 +0900

    profile testcode

--- src/main/java/com/studyolle/demo/settings/Profile.java
+++ src/main/java/com/studyolle/demo/settings/Profile.java
@@ -3,17 +3,22 @@ package com.studyolle.demo.settings;
 import com.studyolle.demo.domain.Account;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @NoArgsConstructor //or default constructor
 public class Profile {
 
+    @Length(max=35)
     private String bio;
 
+    @Length(max=50)
     private String url;
 
+    @Length(max=50)
     private String occupation;
 
+    @Length(max=50)
     private String location; // varchar(255)
 
     public Profile(Account account) {
